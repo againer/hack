f3855ad576dd4a010a72135b5a5e5fb6c714a4c0
Al Pal <superrawr@gmail.com>
2020-08-22 15:11:34 -0700

    Add story item component + layout skeleton.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { StoriesComponent } from './stories/stories.component';
+import { StoryItemComponent } from './story-item/story-item.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, StoriesComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    StoriesComponent,
+    StoryItemComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
