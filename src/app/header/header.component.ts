ff8d953221052d932aaff8a0974128103a06c87d
Al Pal <superrawr@gmail.com>
2020-08-22 12:01:19 -0700

    Flesh out header component some more.
    This adds a `Header` interface that currently includes the `outfit` name-`Moxion News`.  I also added a custom MN logo.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Header } from '../header';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  header: Header = {
+    outfit: 'Moxion News',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
