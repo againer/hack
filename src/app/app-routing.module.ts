9de75ebd84acd00d5f02795a3923786b6419bb57
Al Pal <superrawr@gmail.com>
2020-08-22 12:49:55 -0700

    Add app router for top, best, all stories.
    Top, best and all components are basically skeletons right now--will be fleshed out.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { AllComponent } from './all/all.component';
+import { BestComponent } from './best/best.component';
+import { TopComponent } from './top/top.component';
+
+const routes: Routes = [
+  { path: 'all', component: AllComponent },
+  { path: 'best', component: BestComponent },
+  { path: 'top', component: TopComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { AllComponent } from './all/all.component';
+import { BestComponent } from './best/best.component';
+import { TopComponent } from './top/top.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, AllComponent, BestComponent, TopComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
