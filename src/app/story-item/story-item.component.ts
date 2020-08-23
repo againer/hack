2e3877373545a0527b01081c686f38b06c119df9
Al Pal <superrawr@gmail.com>
2020-08-23 16:57:52 -0700

    Add createDomain helper for story item component.

--- src/app/story-item/story-item.component.ts
+++ src/app/story-item/story-item.component.ts
@@ -17,23 +17,26 @@ export class StoryItemComponent implements OnInit {
 
   constructor(private storiesService: StoriesService) {}
 
+  // It is possible for stories to exclude an external url.
+  // For that case, we need to point to MN's story detail.
+  createDomain(url) {
+    if (!url) {
+      return;
+    }
+
+    const [start, middle, end] = new URL(url).hostname.split('.');
+    return end ? `${middle}.${end}` : `${start}.${middle}`;
+  }
+
   ngOnInit(): void {
     this.storiesService.getStory(this.storyId).subscribe(
-      story => {
-        // It is possible for stories to exclude an external url.
-        // For that case, we need to point to MN's story detail.
-        // TODO (againer): Add default URL for self posts.
-        const [start, middle, end] = story.url
-          ? new URL(story.url).hostname.split('.')
-          : ['test', 'test', 'test'];
-
+      story =>
         // Decorate `story` with `domain` and `timeAgo` for display purposes.
-        this.story = {
+        (this.story = {
           ...story,
-          domain: end ? `${middle}.${end}` : `${start}.${middle}`,
+          domain: this.createDomain(story.url),
           timeAgo: moment.unix(story.time).fromNow(),
-        };
-      },
+        }),
       error => console.log(`error snagging story ${this.storyId}`),
     );
   }
