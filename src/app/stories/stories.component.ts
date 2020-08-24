ad98a6d9218e1746f2485e2cd5033811869cb26a
Al Pal <superrawr@gmail.com>
2020-08-23 20:17:14 -0700

    Add comment about story id "caching".

--- src/app/stories/stories.component.ts
+++ src/app/stories/stories.component.ts
@@ -37,6 +37,10 @@ export class StoriesComponent implements OnInit {
 
       this.storiesService.getStories(this.storyType).subscribe(storyIds => {
         this.page = queryPage >= 0 ? queryPage : 1;
+        // A little bit hacky - instead of hitting the stories API
+        // each time there is a query param change, refer to the existing
+        // cache of storyIds and slice that list.  If the cache isn't
+        // populated, then populate it with stories from HN's API.
         if (this.allStoryIds.length) {
           this.storyIds = this.allStoryIds.slice(
             (this.page - 1) * 30,
